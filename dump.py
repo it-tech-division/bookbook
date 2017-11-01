e7ae3968371375a974e568bbe732139d3d32635c
sungduhong <sdhong@kia.com>
2017-11-01 17:48:53 +0900

    uploading dump utility

--- dump.py
+++ dump.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import urllib.request
 import json, sys
 import pymysql
@@ -33,15 +35,17 @@ def get_bookinfo(query, count=5):
 			book['isbn'] = book['isbn'].split()[1]
 		return result['items']
 
-def insert_book(book, no):
+
+def insert_book(query):
 	conn = pymysql.connect(host=HOST, user=DB_USER, password=DB_PWD, db=DB_NAME, charset='utf8')
 	cur = conn.cursor(pymysql.cursors.DictCursor)
-	sql = "INSERT INTO book_info(NO,TITLE,ISBN,AUTHOR,DESCRIPTION,IMAGE,LINK,CREATEDATA) values('%s','%s','%s','%s','%s','%s','%s',CURDATE())"
-#	print(sql % (no, book['title'], book['isbn'], book['author'], book['description'], book['image'], book['link']))
-	cur.execute(sql % (no, book['title'], book['isbn'], book['author'], book['description'], book['image'], book['link']))
+	sql = "INSERT INTO book_info(title,isbn,author,image,link,register,register_email,created_date) values(%s,%s,%s,%s,%s,%s,%s,CURDATE())"
+	query['name'] = '홍길동'
+	query['email'] = 'aa@hyundai.com'
+#	print (query['title'],query['isbn'],query['author'],query['image'],query['link'],query['name'],query['email'])
+	cur.execute(sql,(query['title'],query['isbn'],query['author'],query['image'].split('?')[0],query['link'],query['name'],query['email']))
+	conn.commit()
 	conn.close()
-
-#	INSERT INTO book_info(NO,TITLE,ISBN,AUTHOR,DESCRIPTION,IMAGE,LINK,CREATEDATA) values(%s,%s,%s,%s,%s,%s,%s,CURDATE()) (8, 'Python 프로그래밍 정복하기 창의적 사고를 위한', '9791185123554', '강남오|김재호', '『창의적 사고를 위한 <b>PYTHON</b> 프로그래밍 정복하기』는 처음으로 컴퓨터 프로그래밍을 학습하는 컴퓨터공학 비전공자 및 저학년의 컴퓨터공학 전공자를 위해 작성되었다. 따라서 컴퓨터 프로그래밍을 학습하는 초보자들이 반드시 알아야할 프로그래밍의 개념과 문법적 표현을 중심으로 다루었다. 뿐만 아니라... ', 'http://bookthumb.phinf.naver.net/cover/102/002/10200299.jpg?type=m1&udate=20160218', 'http://book.naver.com/bookdb/book_detail.php?bid=10200299')
 
 def search_book(string, type):
 	conn = pymysql.connect(host=HOST, user=DB_USER, password=DB_PWD, db=DB_NAME, charset='utf8')
@@ -53,7 +57,7 @@ def search_book(string, type):
 
 
 if __name__ == '__main__':
-	for no, book in enumerate(get_bookinfo('Python', 10)):
+	for no, book in enumerate(get_bookinfo(sys.argv[1], int(sys.argv[2]))):
 		print('=' * 30)
 		print('''
 title : %(title)s
@@ -62,4 +66,4 @@ author : %(author)s
 image : %(image)s
 link : %(link)s
 ''' % book)
-		insert_book(book, no+1)
+		insert_book(book)
