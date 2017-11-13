4852c4f5893e518ef7a299714e980cb617fd8449
eeYeonji <younji1122@naver.com>
2017-11-13 18:07:28 +0900

    app.py

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ def index():
 	book_list = search_book("%","title","0","9")
 	return render_template('home.html', books=book_list)
 
+@app.route('/_scroll')
+def _scroll():
+	book_list = search_book("%","title","10","20")
+	return render_template('bookList.html', books=book_list)
+	
+	
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -37,7 +43,7 @@ def search():
 	query = request.form['query'].lower()
 	start = request.form['start']
 	end = request.form['end']
-	book_list = search_book(query, "title", start, end)
+	book_list = search_book(query, "title", "0", "9")
 	if book_list:
 		messages=""
 	else:
@@ -57,7 +63,7 @@ def regist_book():
 	#print(query)
 	insert_book(query)
 	messages=query['title']+" 책 등록 성공"
-	book_list = search_book("%","title")
+	book_list = search_book("%","title", "0", "10")
 	return render_template('home.html', books=book_list,alert_messages=messages)
 
 @app.route('/borrow_book', methods=['POST','GET'])
@@ -90,11 +96,28 @@ def return_book_procees1():
 	messages=query['title']+" 반납신청 완료"
 	book_list = search_book("%","title")
 	return render_template('home.html', books=book_list,alert_messages=messages)
+	
+# 회원가입
+@app.route('/regist_user', methods=['POST','GET'])
+def regist_user():
+	query=request.form
+	print(query)
+	insert_user(query)
+	messages=query['name']+"님, 부끄부끄의 새가족이 되신것을 환영합니다."
+	return render_template('loginForm.html')
 
 # test
 @app.route('/mailform')
 def mailform():
     return render_template('mailForm.html')
+	
+@app.route('/loginform')
+def loginform():
+    return render_template('loginForm.html')
+	
+@app.route('/registUser')
+def registUser():
+    return render_template('registUser.html')
 	
 	
 if __name__ == '__main__':
